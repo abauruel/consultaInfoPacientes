18a7d4187af629b9b1bf74b29bbffc38b5c81a7f
Alex Claude <abauruel@gmail.com>
2018-11-19 16:40:33 -0200

    Update dbconfig.js
    removido dados restritos

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    user: process.env.NODE_ORACLEDB_USER || "dba_clinica",
-    password : process.env.NODE_ORACLEDB_PASSWORD || "oscet2010",
+    user: process.env.NODE_ORACLEDB_USER || "",
+    password : process.env.NODE_ORACLEDB_PASSWORD || "",
     connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "srv-orcl01/multimed"
 
 }
